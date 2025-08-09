663d39501e3c4be506603677f884f45caf2b2771
Daniel-G-Gitonga <gitongadanielgachuhi1@gmail.com>
2025-08-09 11:49:53 +0300

    some updates

--- initial.cpp
+++ initial.cpp
@@ -2,7 +2,13 @@
 
 int main(int argc, char** argv){
 
-printf("one week before exams 2.1 kaboom.... haha");
-std::endl;
+printf("one week before exams 2.1 kaboom.... haha \n");
+
+for(int i=0;i<=10; i++){
+
+printf("%s \n",argv[i]);
+
+}
+
 return 0;
 }
